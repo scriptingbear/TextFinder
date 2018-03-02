d0f58812c5747348da43abdf705a5dc689283fad
scriptingbear <adiv.abramson@yahoo.com>
2018-03-01 18:17:12 -0600

    Latest version

---
+++ TextFinder/frmTextFinder.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace TextFinder
+{
+    partial class frmTextFinder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTextFinder));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.searchTerm = new System.Windows.Forms.TextBox();
+            this.inputText = new System.Windows.Forms.RichTextBox();
+            this.foundItems = new System.Windows.Forms.ListView();
+            this.colText = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colStart = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colEnd = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.resetFoundItems = new System.Windows.Forms.Button();
+            this.deleteSearchBox = new System.Windows.Forms.Button();
+            this.deleteInputText = new System.Windows.Forms.Button();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Search For";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Input";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(505, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Found Items";
+            // 
+            // searchTerm
+            // 
+            this.searchTerm.Location = new System.Drawing.Point(107, 91);
+            this.searchTerm.Name = "searchTerm";
+            this.searchTerm.Size = new System.Drawing.Size(374, 27);
+            this.searchTerm.TabIndex = 0;
+            this.toolTip1.SetToolTip(this.searchTerm, "Type in text and press Enter to search");
+            this.searchTerm.TextChanged += new System.EventHandler(this.searchTerm_TextChanged);
+            this.searchTerm.KeyDown += new System.Windows.Forms.KeyEventHandler(this.searchTerm_KeyDown);
+            // 
+            // inputText
+            // 
+            this.inputText.Location = new System.Drawing.Point(12, 161);
+            this.inputText.Name = "inputText";
+            this.inputText.Size = new System.Drawing.Size(469, 279);
+            this.inputText.TabIndex = 2;
+            this.inputText.Text = "";
+            this.toolTip1.SetToolTip(this.inputText, "Type or paste in text to be searched");
+            // 
+            // foundItems
+            // 
+            this.foundItems.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colText,
+            this.colStart,
+            this.colEnd});
+            this.foundItems.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.foundItems.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.foundItems.Location = new System.Drawing.Point(499, 161);
+            this.foundItems.MultiSelect = false;
+            this.foundItems.Name = "foundItems";
+            this.foundItems.Size = new System.Drawing.Size(300, 279);
+            this.foundItems.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.foundItems.TabIndex = 4;
+            this.toolTip1.SetToolTip(this.foundItems, "Click an item in the list to highlight it in the Input box\r\nDouble click to chang" +
+        "e selected item");
+            this.foundItems.UseCompatibleStateImageBehavior = false;
+            this.foundItems.View = System.Windows.Forms.View.Details;
+            this.foundItems.SelectedIndexChanged += new System.EventHandler(this.foundItems_SelectedIndexChanged);
+            this.foundItems.Click += new System.EventHandler(this.foundItems_Click);
+            this.foundItems.DoubleClick += new System.EventHandler(this.foundItems_DoubleClick);
+            // 
+            // colText
+            // 
+            this.colText.Text = "Text";
+            this.colText.Width = 175;
+            // 
+            // colStart
+            // 
+            this.colStart.Text = "Start";
+            this.colStart.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // colEnd
+            // 
+            this.colEnd.Text = "End";
+            this.colEnd.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // resetFoundItems
+            // 
+            this.resetFoundItems.Location = new System.Drawing.Point(705, 126);
+            this.resetFoundItems.Name = "resetFoundItems";
+            this.resetFoundItems.Size = new System.Drawing.Size(94, 29);
+            this.resetFoundItems.TabIndex = 5;
+            this.resetFoundItems.Text = "Reset";
+            this.toolTip1.SetToolTip(this.resetFoundItems, "Clear Found Items list");
+            this.resetFoundItems.UseVisualStyleBackColor = true;
+            this.resetFoundItems.Click += new System.EventHandler(this.resetFoundItems_Click);
+            // 
+            // deleteSearchBox
+            // 
+            this.deleteSearchBox.Font = new System.Drawing.Font("Wingdings", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.deleteSearchBox.Location = new System.Drawing.Point(487, 92);
+            this.deleteSearchBox.Name = "deleteSearchBox";
+            this.deleteSearchBox.Size = new System.Drawing.Size(45, 26);
+            this.deleteSearchBox.TabIndex = 1;
+            this.deleteSearchBox.Text = "ý";
+            this.toolTip1.SetToolTip(this.deleteSearchBox, "Clear Search For box");
+            this.deleteSearchBox.UseVisualStyleBackColor = true;
+            this.deleteSearchBox.Click += new System.EventHandler(this.deleteSearchBox_Click);
+            // 
+            // deleteInputText
+            // 
+            this.deleteInputText.Font = new System.Drawing.Font("Wingdings", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.deleteInputText.Location = new System.Drawing.Point(436, 132);
+            this.deleteInputText.Name = "deleteInputText";
+            this.deleteInputText.Size = new System.Drawing.Size(45, 26);
+            this.deleteInputText.TabIndex = 3;
+            this.deleteInputText.Text = "ý";
+            this.toolTip1.SetToolTip(this.deleteInputText, "Clear Input box");
+            this.deleteInputText.UseVisualStyleBackColor = true;
+            this.deleteInputText.Click += new System.EventHandler(this.deleteInputText_Click);
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.AutoSize = true;
+            this.btnAbout.Location = new System.Drawing.Point(730, 12);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(77, 37);
+            this.btnAbout.TabIndex = 6;
+            this.btnAbout.Text = "About...";
+            this.btnAbout.UseVisualStyleBackColor = true;
+            this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(14, 7);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(496, 64);
+            this.label4.TabIndex = 7;
+            this.label4.Text = resources.GetString("label4.Text");
+            // 
+            // frmTextFinder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 452);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.deleteInputText);
+            this.Controls.Add(this.deleteSearchBox);
+            this.Controls.Add(this.resetFoundItems);
+            this.Controls.Add(this.foundItems);
+            this.Controls.Add(this.inputText);
+            this.Controls.Add(this.searchTerm);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmTextFinder";
+            this.Text = "Text Finder";
+            this.Load += new System.EventHandler(this.frmTextFinder_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox searchTerm;
+        private System.Windows.Forms.RichTextBox inputText;
+        private System.Windows.Forms.ListView foundItems;
+        private System.Windows.Forms.ColumnHeader colText;
+        private System.Windows.Forms.ColumnHeader colStart;
+        private System.Windows.Forms.ColumnHeader colEnd;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button resetFoundItems;
+        private System.Windows.Forms.Button deleteSearchBox;
+        private System.Windows.Forms.Button deleteInputText;
+        private System.Windows.Forms.Button btnAbout;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ TextFinder/frmTextFinder.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using Microsoft.VisualBasic;
+
+namespace TextFinder
+{
+    public partial class frmTextFinder : Form
+    {
+        public frmTextFinder()
+        {
+            InitializeComponent();
+        }
+
+        private void frmTextFinder_Load(object sender, EventArgs e)
+        {
+
+        }//frmTextFinder_Load()
+
+        private void searchTerm_KeyDown(object sender, KeyEventArgs e)
+        {
+            //search for text when Enter key has been pressed
+            if (e.KeyCode == Keys.Enter)
+            {
+                var start = 0;
+                var searchTermLength = 0;
+                var searchTermValue = "";
+                var selectedTextValue = "";
+                
+
+                searchTermValue = searchTerm.Text;
+
+                if ((searchTermValue.Trim() == string.Empty) || 
+                        (inputText.Text == string.Empty))
+                {   //can't search on just spaces
+                    //and can't search on no input
+                    return;
+                }
+
+                ClearInput();
+
+                searchTermLength = searchTermValue.Length;
+                do
+                {
+                    //find index of next occurence of search term
+                    start = inputText.Find(searchTermValue, start, RichTextBoxFinds.None);
+                    if (start != -1)
+                    {
+                        inputText.Select(start, searchTermLength);
+                        inputText.SelectionBackColor = Color.Yellow;
+                        //write the value of the found item, not the original 
+                        //search item, since we have selected case insensitive
+                        //seach
+                        selectedTextValue = inputText.SelectedText;
+                        AddItem(selectedTextValue, start, searchTermLength);
+                        start += searchTermLength;
+                        if (start >= inputText.Text.Length)
+                        {
+                            break;
+                        }
+                    }
+
+                } while (start != -1);
+
+
+            }//(e.KeyCode == Keys.Enter)
+        }//searchTerm_KeyDown
+
+        /// <summary>
+        /// Adds text highlighted item to ListView control
+        /// along with starting and ending positions
+        /// Can't use a ListBox since it doesn't support
+        /// multiple columns like a table would
+        /// </summary>
+        private void AddItem(string value, int start, int len)
+        {
+            //first column of new row is main item
+            //subsequent columns in same row are subitems
+            //Add a new row using ListView.Items.Add(new ListViewItem())
+            //In ListViewItem constructor declare a new string array
+            //and follow that with an array literal {}
+            var end = start + len;
+            foundItems.Items.Add(new ListViewItem(new string[] 
+                             { value, start.ToString(), end.ToString() }));
+        }
+
+        private void ClearInput()
+        {
+            var tempText = "";
+            //clear highlighting, if any, from previous search
+            tempText = inputText.Text;
+            inputText.Clear();
+            inputText.Text = tempText;
+        }
+
+        private void foundItems_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //having problems getting SelectedItems. .NET says its null
+            //and doesn't have any items. Same for FocusItem and
+            //SelectedIndices :-(
+            //Having better luck using the Click() method
+        }
+
+        private void foundItems_Click(object sender, EventArgs e)
+        {
+            //ensure inputText is not empty and that its length is
+            //at least start + item length
+            //can't do anything if the user has tampered with the inpuText 
+
+            if (foundItems.SelectedItems.Count == 0)
+            {
+                return;
+            } 
+
+            var value = foundItems.SelectedItems[0].SubItems[0].Text;
+            var start = int.Parse(foundItems.SelectedItems[0].SubItems[1].Text);
+            var end = int.Parse(foundItems.SelectedItems[0].SubItems[2].Text);
+            var len = end - start;
+
+  
+            if (inputText.Text.Length == 0)
+            {
+                return;
+            }
+            else if (inputText.Text.Length < start + len)
+            {
+                return;
+            }
+
+            //select the item 
+            inputText.Select(start, len);
+            //must set focus or you won't see the highlight
+            inputText.Focus();
+
+        }//foundItems_Click()
+
+        private void searchTerm_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }//searchTerm_TextChanged()
+
+        private void foundItems_DoubleClick(object sender, EventArgs e)
+        {
+            //replace the selected item
+            if (foundItems.SelectedItems.Count == 0)
+            {
+                return;
+            }
+
+            var value = foundItems.SelectedItems[0].SubItems[0].Text;
+            var start = int.Parse(foundItems.SelectedItems[0].SubItems[1].Text);
+            var end = int.Parse(foundItems.SelectedItems[0].SubItems[2].Text);
+            var len = end - start;
+            var replaceValue = "";
+
+            replaceValue = 
+                Microsoft.VisualBasic.Interaction.InputBox("Enter replacement text", "Replace", value);
+            if ((replaceValue != string.Empty) && (replaceValue !=value))
+            {
+                inputText.SelectedText = replaceValue;
+            }
+
+
+        }//foundItems_DoubleClick()
+
+        private void resetFoundItems_Click(object sender, EventArgs e)
+        {
+            //to remove all items from the list, use Items.Clear()
+            //don't call Clear() on the ListView object itself or all columns
+            //will be deleted, too!
+            foundItems.Items.Clear();
+        }//resetFoundItems_Click()
+
+        private void deleteSearchBox_Click(object sender, EventArgs e)
+        {
+            searchTerm.Clear();
+            ClearInput();
+
+        }//deleteSearchBox_Click()
+
+        private void deleteInputText_Click(object sender, EventArgs e)
+        {
+            inputText.Clear();
+            //clear list of found items since input box now is empty
+            //so the items in Found Items no longer reference the original
+            //text
+            foundItems.Items.Clear();
+        } // deleteInputText_Click()
+
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Text Finder is a C# Windows Forms application " +
+                "that does almost nothing useful. You're welcome.",
+                "Text Finder 2018 Professional", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }//class frmTextFinder : Form
+}//TextFinder
